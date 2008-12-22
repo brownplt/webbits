25dd1fe2edb1b5ba7095f4dbb3f5fd3e3ca8aa26
Arjun Guha <arjun@cs.brown.edu>
2008-12-22 14:16:04 -0500

    infrastructure for parse-pretty identity testing

--- Setup.lhs
+++ Setup.lhs
@@ -6,20 +6,19 @@
 
 > isHaskellFile file = ".lhs" `L.isSuffixOf` file || ".hs" `L.isSuffixOf` file
 
-> moduleName file = "Test." ++ m where
->   m = L.takeWhile (\ch -> ch /= '.') file
+> moduleName file = L.takeWhile (\ch -> ch /= '.') file
 
 > testMain _ _ _ _ = do
->   files <- getDirectoryContents "src/Test"
+>   files <- getDirectoryContents "tests"
 >   let tests = filter isHaskellFile files
 >   let testModules = map moduleName tests
 >   let testFuncs = map (++ ".main") testModules
 >   let testExpr = "sequence [ " ++ concat (L.intersperse "," testFuncs) ++ 
 >                  " ] >>= \\cases -> runTestTT (TestList cases)"
 >   let moduleLine = concat (L.intersperse " " testModules)
->   let cmd = "cd src && ghc  -fno-monomorphism-restriction -fglasgow-exts " ++
->             "-package HUnit -e \"" ++ testExpr ++ " >> return ()\" " ++ 
->             moduleLine
+>   let cmd = "cd tests && ghc  -XNoMonomorphismRestriction -fglasgow-exts " ++
+>             "-package HUnit -i:../src -e \"" ++ testExpr ++ 
+>             " >> return ()\" " ++ moduleLine
 >   putStrLn "Testing command is:"
 >   putStrLn cmd
 >   putStrLn "\nLoading tests..."
---
+++ tests/ParsePretty.hs
@@ -0,0 +1,45 @@
+module ParsePretty where
+
+import Test.HUnit.Base
+import Test.HUnit.Text
+import Data.Data
+import System.Directory (getDirectoryContents)
+import System.FilePath (takeExtension, FilePath, (</>))
+
+import WebBits.Common (pp)
+import WebBits.JavaScript.PrettyPrint () -- instances only
+import WebBits.JavaScript.Syntax (JavaScript (..))
+import WebBits.JavaScript.Parser (parseScriptFromString, ParsedStatement)
+import Text.PrettyPrint.HughesPJ (render, vcat)
+
+
+pretty :: [ParsedStatement] -> String
+pretty stmts = render $ vcat $ map pp stmts
+
+parse :: FilePath -> String -> [ParsedStatement]
+parse src str = case parseScriptFromString src str of
+  Left err -> error (show err)
+  Right (Script _ stmts) -> stmts
+
+eraseSourcePos x = fmap (\_ -> ()) x
+
+assertEqualWithoutSourcePos lhs rhs = 
+  case eraseSourcePos lhs == eraseSourcePos rhs of
+    True -> return ()
+    False -> assertFailure $ "expected:\n" ++ show lhs ++ "\ngot:\n" ++
+                             show rhs ++ "\n"
+
+testParsePrettyIdentity :: FilePath -> String -> Test
+testParsePrettyIdentity src str = TestCase $ 
+  assertEqualWithoutSourcePos lhs rhs where
+    lhs = ((parse src) . pretty . (parse src)) str
+    rhs = parse src str
+  
+
+main = do
+  allPaths <- getDirectoryContents "parse-pretty"
+  let testPaths = map ("parse-pretty"</>)
+                    $ filter ((== ".js") . takeExtension) allPaths
+  testData <- mapM readFile testPaths
+  let tests = zipWith testParsePrettyIdentity testPaths testData
+  return (TestList tests)
