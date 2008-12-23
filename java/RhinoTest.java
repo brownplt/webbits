2dd131503f14583ff6128d9cb67a9748b6b69d18
Arjun Guha <arjun@cs.brown.edu>
2008-12-23 09:48:07 -0500

    RhinoTest reads from stdin if no args given; first try guessed the class name

--- java/RhinoTest.java
+++ java/RhinoTest.java
@@ -5,19 +5,20 @@ import org.mozilla.javascript.*;
 
 public final class RhinoTest {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length > 1) {
             System.err.println("usage: RhinoTest [FILE]");
             System.exit(1);
         }
 
         try {
-            File f = new File(args[0]);
-            Reader r = new FileReader(f);
+            String src = args.length == 1 ? args[0] : "stdin";
+            Reader r = args.length == 1 ? new FileReader(new File(args[0]))
+                                        : new InputStreamReader(System.in);
 
             ContextFactory cf = new ContextFactory(); // I hate factories
 
             Context c = cf.enterContext();
-            Script  s = c.compileReader(r, args[0], 1, null);
+            Script  s = c.compileReader(r, src, 1, null);
 
             System.out.println(c.decompileScript(s, 4));
         } catch (IOException e) {
