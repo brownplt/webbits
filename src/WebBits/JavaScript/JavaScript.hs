ef60060584e91013d6753fb3b647f1497e2ead0c
Spiridon Eliopoulos <seliopou@gmail.com>
2008-12-26 19:01:20 -0500

    Export WebBits.Common in the JavaScript module.

--- src/WebBits/JavaScript/JavaScript.hs
+++ src/WebBits/JavaScript/JavaScript.hs
@@ -4,10 +4,12 @@ module WebBits.JavaScript.JavaScript
   , module WebBits.JavaScript.HtmlEmbedding
   , module WebBits.JavaScript.Parser
   , module WebBits.JavaScript.Combinators
+  , module WebBits.Common
   -- JavaScript.Instances exports nothing
   ) where
 
--- 
+import WebBits.Common
+
 import WebBits.JavaScript.Syntax
 import WebBits.JavaScript.Parser
 import WebBits.JavaScript.PrettyPrint
