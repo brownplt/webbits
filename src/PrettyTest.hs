370c48eaed11fab83622089a7dd0499532a5ba23
Andrey Chudnov <oss@chudnov.com>
2013-05-28 15:09:08 -0400

    Refactoring of the PrettyPrint module; added the new class Pretty for pretty-printing ASTs which supercedes all the other interfaces for the same purpose

--- src/PrettyTest.hs
+++ src/PrettyTest.hs
@@ -3,13 +3,12 @@ module Main where
 import Language.ECMAScript3.Parser
 import Language.ECMAScript3.PrettyPrint
 import Language.ECMAScript3.Syntax
-import Language.ECMAScript3.Syntax.Arbitrary
+import Language.ECMAScript3.Syntax.Arbitrary()
 import Language.ECMAScript3.Syntax.Annotations
 import Test.QuickCheck
 import System.Exit
 
-
-
+main :: IO ()
 main = 
   let qcArgs = Args {maxSuccess = 50
                     ,maxDiscardRatio = 10
@@ -25,6 +24,6 @@ main =
         
 prettyParseEquivalence :: JavaScript () -> Bool
 prettyParseEquivalence js = 
-  case parseScriptFromString "" $ show $ javaScript js of
+  case parseScriptFromString "" $ show $ prettyPrint js of
     Left _ -> False
     Right parsed -> (reannotate (const ()) parsed) == js
