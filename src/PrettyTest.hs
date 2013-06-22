17d1747bc721453d73205555a63ed1d64e2e5d9f
Andrey Chudnov <achudnov@stevens.edu>
2013-06-21 22:10:50 -0400

    Output diff for tests

--- src/PrettyTest.hs
+++ src/PrettyTest.hs
@@ -5,8 +5,9 @@ import Language.ECMAScript3.PrettyPrint
 import Language.ECMAScript3.Syntax
 import Language.ECMAScript3.Syntax.Arbitrary()
 import Language.ECMAScript3.Syntax.Annotations
-import Test.QuickCheck
 import System.Exit
+import Test.SourceDiff
+import Test.QuickCheck
 
 main :: IO ()
 main = 
@@ -14,16 +15,24 @@ main =
                     ,maxDiscardRatio = 10
                     ,maxSize = 10
                     ,replay = Nothing
-                    ,chatty = True}
+                    ,chatty = False}
   in quickCheckWithResult qcArgs prettyParseEquivalence >>= \res ->
   case res of
     Success {} -> putStrLn "All tests passes"
     GaveUp {} -> putStrLn "Gave up"
     Failure {} -> putStrLn "Test failed" >> exitFailure
     NoExpectedFailure {} -> putStrLn "Unexpected failure" >> exitFailure
-        
-prettyParseEquivalence :: JavaScript () -> Bool
-prettyParseEquivalence js = 
-  case parseScriptFromString "" $ show $ prettyPrint js of
-    Left _ -> False
-    Right parsed -> (reannotate (const ()) parsed) == js
+
+prettyParseEquivalence :: JavaScript () -> Property
+prettyParseEquivalence js =
+  let pp = show $ prettyPrint js
+  in case parseScriptFromString "" pp of
+    Left e -> 
+      let err = "Can't parse pretty-printed code. The error was: " ++ (show e) ++
+                "\nThe pretty-printed code in question:\n" ++ pp
+      in whenFail (putStrLn err) False
+    Right parsed ->
+      let eq = (removeAnnotations parsed) == js
+          msg ="The parse of the pretty-printed AST didn't match the original\n"
+               ++"Diff:\n" ++ jsDiff js (reannotate (const ()) parsed)
+      in whenFail (putStrLn msg) eq
--- src/UnitTest.hs
+++ src/UnitTest.hs
@@ -7,19 +7,31 @@ import qualified System.FilePath as FilePath
 import Language.ECMAScript3.Parser
 import Language.ECMAScript3.PrettyPrint
 import Language.ECMAScript3.Syntax
+import Language.ECMAScript3.Syntax.Annotations
+import Test.SourceDiff
+import Control.Monad
 
 testDir = "tests/parse-pretty"
 
 
 -- | tests the parser with pre-defined test-cases
-parsePrettyTest filename = TestLabel filename $ TestCase $ do
-  js <- parseJavaScriptFromFile filename
-  let str = renderStatements js
-  case parseScriptFromString "" str of
-    Left err -> assertFailure (show err)
-    Right (Script _ js') -> do
-      let str' = renderStatements js'
-      assertBool "pretty-printed code should re-parse" (str == str')
+parsePrettyTest filename = TestLabel filename $ TestCase $ 
+  readFile filename >>= \src ->
+  case parseScriptFromString "" src of
+    Left err -> assertFailure $ "Can't parse a test-case: " ++ filename
+    Right js -> let str = show $ prettyPrint js
+                in  case parseScriptFromString "" str of
+                  Left err ->
+                    let msg = "Can't parse pretty-printed code. The error was: "
+                              ++ (show err)
+                              ++ "\nThe pretty-printed code in question:\n" ++ str
+                    in assertFailure msg
+                  Right js' -> do
+                    let str' = show $ prettyPrint js'
+                    unless (str == str') $ do
+                      let msg = "The parse of the pretty-printed AST didn't match the original\n"
+                                ++ "Diff:\n" ++ jsDiff js js'
+                      assertFailure msg
 
 main = do
   allFiles <- getDirectoryContents testDir
